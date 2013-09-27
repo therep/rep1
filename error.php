07274edc94089bc46bbdbefe650ad0f46bb332b6
Ward McQueen <wamcq@cox.net>
2013-09-27 13:05:32 -0700

    new file 2

---
+++ error.php
@@ -0,0 +1,50 @@
+<?php
+/*	
+	ERROR CODE
+	Author: Joel Aichler
+	Last Updated: 10/12/12
+	Version: 2.0
+*/
+?>
+<!DOCTYPE html>
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1">
+<title>Talent Not Found!</title>
+<link rel="stylesheet" href="style.css" type="text/css">
+</head>
+
+<body>
+<!-- HEADER START -->
+<div id="header">
+	<div id="header2" align="center"><a href="index.html"><img src="images/Logo.jpg" alt="Logo" title="Go to Dani's Agency Homepage" width="420" height="91" border="0" /></a></div>
+	<hr /><br /><br />
+</div>
+<!-- END HEADER -->
+
+<!--START SEARCH RESULTS PANE -->
+<div align="center" style="margin:auto;width=100%;font-size:18px;color:#FF0000;">
+NO SEARCH RESULTS FOUND FOR GIVEN SEARCH!<br />***CLICK THE ARROW <a href="javascript:history.go(-1)">
+<img src="images/arrow_left.png" alt="Click the Back Button on your Browser!"  width="64" height="64" border="0" title="Go Back and Search Again!" /></a> TO SEARCH AGAIN***
+</div>
+<!-- END SEARCH-RESULTS PANE -->
+
+<div align="center" style="width:100%;margin:auto;">
+<p align="center" style="font-size:smaller;color:#FF0000;"><?php echo "0 Records Found"; ?></p>
+</div>
+
+<!-- FOOTER BEGIN -->	  
+<div id="footer">
+	<hr />
+	<p><a href="javascript:history.go(-1)">Talent Database</a> | <a href="http://www.danisagency.com/Female_voice_Eng_mp3.html" target="_top"> English Female Voice Demos</a> | <a href="http://www.danisagency.com/Female_voice_Span_mp3.html" target="_top"> Spanish Female Voice Demos<br />
+	</a><a href="http://www.danisagency.com/Male_voice_Eng_mp3.html" target="_top">English Male Voice Demos</A> | <a href="http://www.danisagency.com/Male_voice_Span_mp3.html" target="_top">Spanish Male Voice Demos</a> | <a href="http://www.danisagency.com/Resources.html" target="_top">Resources</a> | <a href="http://www.danisagency.com/aboutus.html" target="_top">About Us</a>
+	</p>
+	<h6 align="left">Copyright &copy; 2012 Dani's Agency<br />
+	Design & Programming by <a href="mailto:chrono24k@cox.net">Joel</a><br />
+	All rights reserved</h6>
+	<h6>EMPLOYER AND APPLICANT PAID SERVICE</h6>
+</div>
+<!-- FOOTER END -->
+
+</body>
+</html>
